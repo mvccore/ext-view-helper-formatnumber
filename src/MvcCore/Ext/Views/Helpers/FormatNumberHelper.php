d2172a2c9210f764a103a53ae6ce90c6adb56155
Tom Flídr <tomflidr@gmail.com>
2021-01-15 22:46:21 +0100

    Fixed copyright heading, fixed php docs comments with unnecessary interfaces, upgraded version constant to 5.0.0

--- src/MvcCore/Ext/Views/Helpers/FormatNumberHelper.php
+++ src/MvcCore/Ext/Views/Helpers/FormatNumberHelper.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view
  * the LICENSE.md file that are distributed with this source code.
  *
- * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @copyright	Copyright (c) 2016 Tom Flidr (https://github.com/mvccore)
  * @license		https://mvccore.github.io/docs/mvccore/5.0.0/LICENCE.md
  */
 
@@ -36,14 +36,14 @@ namespace MvcCore\Ext\Views\Helpers;
  * @see http://php.net/manual/en/function.localeconv.php
  * @method \MvcCore\Ext\Views\Helpers\FormatNumberHelper GetInstance()
  */
-class FormatNumberHelper extends \MvcCore\Ext\Views\Helpers\InternationalizedHelper
-{
+class FormatNumberHelper extends \MvcCore\Ext\Views\Helpers\InternationalizedHelper {
+
 	/**
 	 * MvcCore Extension - View Helper - Assets - version:
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.0-alpha';
+	const VERSION = '5.0.0';
 
 	/**
 	 * If this static property is set - helper is possible
